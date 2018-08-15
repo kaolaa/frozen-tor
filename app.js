9de3d934eb3ffba336978c8f682578b13f4e3da0
khaoula atrach <khatrach@gmail.com>
2018-08-15 17:52:03 +0100

    index.js

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+//map global promise - get rid of warning
+mongoose.promise = global.promise;
+
+//Handlebars Middleware 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Methode override middleware
+app.use(methodOverride('_method'));
+
+// Express session middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true 
+}));
+
+app.use(flash());
+ 
+// Global variables
+app.use(function(req,res,next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+
+});
 
 app.get('/', (req, res) => {
   res.send('It Works!');
